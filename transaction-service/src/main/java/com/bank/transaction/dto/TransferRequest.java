e9a4a773d5e427a3821acb7549e26f0ac66cdb22
chandrakalyan <113123850+chandrakalyan2108@users.noreply.github.com>
2026-07-26 12:00:43 +0530

    Update TransferRequest.java

--- transaction-service/src/main/java/com/bank/transaction/dto/TransferRequest.java
+++ transaction-service/src/main/java/com/bank/transaction/dto/TransferRequest.java
@@ -1,6 +1,7 @@
 package com.bank.transaction.dto;
 
 import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import java.math.BigDecimal;
@@ -9,8 +10,8 @@ import java.math.BigDecimal;
 public class TransferRequest {
     @NotNull
     private Long fromAccountId;
-    @NotNull
-    private Long toAccountId;
+    @NotBlank
+    private String toAccountNumber;
     @NotNull @DecimalMin(value = "0.01")
     private BigDecimal amount;
     private String remarks;
