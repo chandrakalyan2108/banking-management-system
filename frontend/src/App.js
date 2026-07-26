fe610d4b25270215f66ab8ab88316ccf1f9d8a4a
chandrakalyan <113123850+chandrakalyan2108@users.noreply.github.com>
2026-07-26 18:18:01 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import PrivateRoute from "./components/PrivateRoute";
 import AdminRoute from "./components/AdminRoute";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar2";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
